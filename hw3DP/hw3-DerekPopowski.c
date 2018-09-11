756d1748c6f6216a5f2aa5964fa903062b7fc906
Derek P <>
2018-09-11 13:38:44 -0500

    update hw3.2 added search function -  still needs testing

--- hw3DP/hw3-DerekPopowski.c
+++ hw3DP/hw3-DerekPopowski.c
@@ -13,25 +13,56 @@ hw
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int search(char *data,int dataSize,char *search);
 
 int main(int argc, char* argv[]){
 
   system("wget http://undcemcs01.und.edu/~ronald.marsh/CLASS/CS451/hw3-data.txt");
-  //FILE* file = fopen("hw3-data.txt","r");
+
   int charCount = 0;
   char *Count = malloc(256);
-  FILE *wc_pipe = popen("wc -c hw3-data.txt", "r");
-  //fgets (Count,255,wc_pipe);
-  fscanf(wc_pipe,"%d",&charCount);
-  pclose(wc_pipe);
-       //int i =  system("wc -c hw3-data.txt");
-  printf("from counter: %d \n",charCount);
-  char *buffer = malloc (charCount);
-  int i;
-  FILE *data = fopen("hw3-data.txt", "r");
+  FILE *wc_pipe = popen("wc -c hw3-data.txt", "r");//pipe to use wc for file size (number of characters)
+
+  fscanf(wc_pipe,"%d",&charCount);//get the int value of number of characters
+  pclose(wc_pipe);//close the pipe
+
+  printf("from counter: %d \n",charCount);//verify character count
+  char *buffer = malloc (charCount+1);//buffer to store the contents of the file + 1 for ending null terminator
+  int i;//loop counter
+  FILE *data = fopen("hw3-data.txt", "r");//open the data file for reading
   for (i = 0; i < charCount;i++){
-    fscanf(data,"%c",&buffer[i]);
+    fscanf(data,"%c",&buffer[i]);//copy the file to the buffer 
   }
-  printf("\n%s\n",buffer);
+  buffer[charCount+1] = '\0';//null terminate the string
+  printf("\n%s\n",buffer);//print the buffer the verify the storage is correct
+
+}
+
+int search(char *data, int dataSize, char *search){
+  char *subStr;=malloc(100);//sub string buffer
+  int i,j = 0;//loop counters
+  int wordCount = 0;
+  for(i = 0; i < dataSize; i++){
+    
+    if(data[i] == ' '){
+      subStr[j+1]='\0';//null term the sub string
+      if(strcmp(subStr,search)==0){
+	wordCount++;
+      }
+      j=0;//reset the substring
+      
+    
+    }
+    else{
+      subStr[j] = data[i];
+      j++;
+      
+    }
+
+
+  }
+  return wordCount;
 
 }
