34e640b2433646de5fdefd653548e83246a0ca4a
Derek P <>
2018-09-11 12:04:06 -0500

    mkdir hw3

---
+++ hw3DP/hw3-DerekPopowski.c
@@ -0,0 +1,22 @@
+/*
+Derek Popowski
+
+derek.a.popowski@und.edu
+
+CSCI 451 Operating systems I
+
+hw
+
+
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+  system("wget http://undcemcs01.und.edu/~ronald.marsh/CLASS/CS451/hw3-data.txt");
+
+
+}
