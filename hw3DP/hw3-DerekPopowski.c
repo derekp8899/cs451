546e13adbf8436d25e5391631a4c005f0cadd8dc
Derek P <>
2018-09-17 11:12:45 -0500

    update 3.5 used wget -O and removed unneeded prints and comments

--- hw3DP/hw3-DerekPopowski.c
+++ hw3DP/hw3-DerekPopowski.c
@@ -5,9 +5,9 @@ derek.a.popowski@und.edu
 
 CSCI 451 Operating systems I
 
-hw
+hw - 3 
 
-
+9-17-18
 
 */
 
@@ -16,7 +16,7 @@ hw
 #include <string.h>
 #include <pthread.h>
 
-struct searchArgs{
+struct searchArgs{//struct to pass arguements into the function run as threads
 
   int dataSize;
   char *search;
@@ -28,10 +28,11 @@ void *search(void *searchArgs);
 
 int main(int argc, char* argv[]){
 
-  system("wget http://undcemcs01.und.edu/~ronald.marsh/CLASS/CS451/hw3-data.txt");
+  system("wget -O hw3-data.txt http://undcemcs01.und.edu/~ronald.marsh/CLASS/CS451/hw3-data.txt");
+  //wget the file overwrite if it exists
 
   int charCount = 0;
-  //char *Count = malloc(256);
+
   FILE *wc_pipe = popen("wc -c hw3-data.txt", "r");//pipe to use wc for file size (number of characters)
 
   fscanf(wc_pipe,"%d",&charCount);//get the int value of number of characters
@@ -43,15 +44,15 @@ int main(int argc, char* argv[]){
   FILE *data = fopen("hw3-data.txt", "r");//open the data file for reading
   for (i = 0; i < charCount;i++){
     fscanf(data,"%c",&buffer[i]);//copy the file to the buffer 
-    if(buffer[i]<90&&buffer[i]>65){//convert all upper case to lower case
+    if(buffer[i]<91&&buffer[i]>64){//convert all upper case to lower case
       buffer[i] += 32;
     }
   }
   fclose(data);//close the input file
   buffer[charCount+1] = '\0';//null terminate the string
-  printf("\n%s\n",buffer);//print the buffer the verify the storage is correct
+  //printf("\n%s\n",buffer);//debug print call to  print the buffer the verify the storage is correct
   pthread_t thrd1,thrd2;//create the two pthreads
-  //int t1ret,t2ret;
+
   struct searchArgs search_Args,search_Args2;//create the two search arguments
   search_Args.dataSize = charCount;
   search_Args.data = buffer;
@@ -63,8 +64,6 @@ int main(int argc, char* argv[]){
   pthread_join(thrd1,NULL);
   pthread_join(thrd2,NULL);//wait for both threads to finish
 
-  //search(buffer,charCount,"polar");
-  //search(buffer, charCount,"easy");
   free(buffer);//free the input data buffer
 }
 
@@ -77,28 +76,20 @@ void *search(void *searchArgs){//to search the data from the input file for a gi
   int i,j = 0;//loop counters
   int wordCount = 0;
   for(i = 0; i < dataSize; i++){//to search the entire data from the file
-    
     if(data[i] == ' '){//when encounter the end of a word
       subStr[j]='\0';//null term the sub string
-      //printf("compairing %s with %s \n",search, subStr);
-      if(strcmp(subStr,search)==0){//compare the the desired search string
-
+      //printf("compairing %s with %s \n",search, subStr);//debugging print to check strcomps
+      if(strcmp(subStr,search)==0){//compare with the desired search string
 	wordCount++;
       }
-      j=0;//reset the substring
-      
-    
+      j=0;//reset the substring          
     }
     else{
       subStr[j] = data[i];//build the current word
-      j++;
-      
+      j++;      
     }
-
-
   }
   printf("count of %s in the file : %d\n", search,wordCount);//print the number of occurances found in the file
   free(subStr);//free the substring buffer
   return;
-
 }
